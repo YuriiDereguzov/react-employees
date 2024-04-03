2229eeaf32cca7a7c6ac32e6dd46eb32fc349145
Yurii Dereguzov <DereguzovYE@yandex.ru>
2024-04-03 23:28:06 +0500

    feat: Сделать контроллеры для employees

--- routes/employees.js
+++ routes/employees.js
@@ -1,20 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { auth } = require('../middleware/auth');
+const { auth } = require("../middleware/auth");
+const {
+  all,
+  add,
+  remove,
+  edit,
+  employee,
+} = require("../controllers/employees");
 
 // /api/employees
-router.get('/', auth, () => console.log('get all employees'));
+router.get("/", auth, all);
 
 // /api/employees/:id
-router.get('/:id', auth, () => console.log('get single employees'));
+router.get("/:id", auth, employee);
 
 // /api/employees/add
-router.post('/add', auth, () => console.log('add employee'));
+router.post("/add", auth, add);
 
 // /api/employees/remove/:id
-router.post('/remove/:id', auth, () => console.log('remove employee'));
+router.post("/remove/:id", auth, remove);
 
 // /api/employees/edit/:id
-router.put('/edit/:id', auth, () => console.log('edit employee'));
+router.put("/edit/:id", auth, edit);
 
-module.exports = router
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { login, register, current} = require('../controllers/users');
-const { auth } = require('../middleware/auth');
+const { login, register, current } = require("../controllers/users");
+const { auth } = require("../middleware/auth");
 
 // /api/user/login
-router.post('/login', login);
+router.post("/login", login);
 
 // /api/user/register
-router.post('/register', register);
+router.post("/register", register);
 
 // /api/user/current
-router.get('/current', auth, current);
+router.get("/current", auth, current);
 
 module.exports = router;
