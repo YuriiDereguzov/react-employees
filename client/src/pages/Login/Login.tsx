e70a81e299093bd26962f491b49c866a07a9ed5f
Yurii Dereguzov <DereguzovYE@yandex.ru>
2024-05-07 16:35:29 +0500

    feat: подключение RTK Query

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import { useState } from "react";
 import { Layout } from "../../conponents/Layout/Layout";
 import { Card, Form, Row, Space, Typography } from "antd";
 import { CustomInput } from "../../conponents/CustomInput/CustomInput";
 import { PasswordInput } from "../../conponents/PasswordInput/PasswordInput";
 import { CustomButton } from "../../conponents/CustomButtom/CustomButton";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Paths } from "../../paths";
+import { UserData, useLoginMutation } from "../../app/services/auth";
+import { isErrorsWithMassage } from "../../utils/isErrorWithMassage";
+import { ErrorMessage } from "../../conponents/ErrorMassage/ErrorMessage";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  const [loginUser, loginUserResult] = useLoginMutation();
+  const [error, serError] = useState("");
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap();
+
+      navigate("/");
+    } catch (err) {
+      const maybeError = isErrorsWithMassage(err);
+
+      if (maybeError) {
+        serError(err.data.message);
+      } else {
+        serError("Неизвестная ошибка");
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align={"middle"} justify={"center"}>
         <Card title="Вход" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={login}>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
             <CustomButton type="primary" htmlType="submit">
@@ -23,6 +46,7 @@ export const Login = () => {
             <Typography.Text>
               Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
