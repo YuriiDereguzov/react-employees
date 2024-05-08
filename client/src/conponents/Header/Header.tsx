4caa32fb31866b8e42a5395da54431a9606e62eb
Yurii Dereguzov <DereguzovYE@yandex.ru>
2024-05-08 17:24:02 +0500

    feat: Добавить кнопку выхода из аккаунта

--- client/src/conponents/Header/Header.tsx
+++ client/src/conponents/Header/Header.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LoginOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
 import { Layout, Space, Typography } from "antd";
 import { CustomButton } from "../CustomButtom/CustomButton";
 import { Paths } from "../../paths";
-
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../../features/auth/authSlice";
 import styles from "./Header.module.css";
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const onLogoutClick = () => {
+    dispatch(logout());
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <Layout.Header className={styles.header}>
       <Space>
@@ -18,14 +29,28 @@ export const Header = () => {
           </CustomButton>
         </Link>
       </Space>
-      <Space>
-        <Link to={Paths.register}>
-          <CustomButton type="ghost" icon={<UserOutlined />}>Зарегистрировать</CustomButton>
-        </Link>
-        <Link to={Paths.login}>
-          <CustomButton type="ghost" icon={<LoginOutlined />}>Войти</CustomButton>
-        </Link>
-      </Space>
+      {user ? (
+        <CustomButton
+          type="ghost"
+          icon={<LoginOutlined />}
+          onClick={onLogoutClick}
+        >
+          Выйти
+        </CustomButton>
+      ) : (
+        <Space>
+          <Link to={Paths.register}>
+            <CustomButton type="ghost" icon={<UserOutlined />}>
+              Зарегистрировать
+            </CustomButton>
+          </Link>
+          <Link to={Paths.login}>
+            <CustomButton type="ghost" icon={<LoginOutlined />}>
+              Войти
+            </CustomButton>
+          </Link>
+        </Space>
+      )}
     </Layout.Header>
   );
 };
